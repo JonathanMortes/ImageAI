06bf340a9fa5029b5861da152f9bfc679e25840f
Jonathan Mortes Esquer <jony-jme@hotmail.com>
2021-02-27 14:02:01 +0100

    here we go again...

--- imageai/Detection/Custom/gen_anchors.py
+++ imageai/Detection/Custom/gen_anchors.py
@@ -1,9 +1,8 @@
 import random
-import argparse
 import numpy as np
 
 from imageai.Detection.Custom.voc import parse_voc_annotation
-import json
+
 
 def IOU(ann, centroids):
     w, h = ann
@@ -23,6 +22,7 @@ def IOU(ann, centroids):
         similarities.append(similarity) # will become (k,) shape
 
     return np.array(similarities)
+
 
 def avg_IOU(anns, centroids):
     n,d = anns.shape
@@ -69,6 +69,7 @@ def run_kmeans(ann_dims, anchor_num):
         prev_assignments = assignments.copy()
         old_distances = distances.copy()
 
+
 def generateAnchors(train_annotation_folder, train_image_folder, train_cache_file, model_labels):
 
     print("Generating anchor boxes for training images and annotation...")
@@ -87,8 +88,8 @@ def generateAnchors(train_annotation_folder, train_image_folder, train_cache_fil
 
         for obj in image['object']:
             relative_w = (float(obj['xmax']) - float(obj['xmin']))/image['width']
-            relatice_h = (float(obj["ymax"]) - float(obj['ymin']))/image['height']
-            annotation_dims.append(tuple(map(float, (relative_w,relatice_h))))
+            relative_h = (float(obj["ymax"]) - float(obj['ymin']))/image['height']
+            annotation_dims.append(tuple(map(float, (relative_w,relative_h))))
 
     annotation_dims = np.array(annotation_dims)
     centroids = run_kmeans(annotation_dims, num_anchors)
@@ -101,7 +102,6 @@ def generateAnchors(train_annotation_folder, train_image_folder, train_cache_fil
     widths = anchors[:, 0]
     sorted_indices = np.argsort(widths)
 
-
     anchor_array = []
     reverse_anchor_array = []
     out_string = ""
@@ -112,11 +112,9 @@ def generateAnchors(train_annotation_folder, train_image_folder, train_cache_fil
 
         out_string += str(int(anchors[i, 0] * 416)) + ',' + str(int(anchors[i, 1] * 416)) + ', '
 
-
-    reverse_count = len(anchor_array) -1
-    while(reverse_count > -1):
-        reverse_anchor_array.append(anchor_array[reverse_count])
-        reverse_count -= 1
+    reverse_anchor_array.append(anchor_array[12:18])
+    reverse_anchor_array.append(anchor_array[6:12])
+    reverse_anchor_array.append(anchor_array[0:6])
 
     print("Anchor Boxes generated.")
     return anchor_array, reverse_anchor_array
